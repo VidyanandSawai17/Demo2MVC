2422ada26decbaf71812c473df19bb3783c37b19
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-04-05 19:08:30 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Demo2MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo2MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee emp)
+        {
+            return View();
+        }
+
+        public IActionResult NewEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewEmployee(Employee emp)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo2MVC.Models
+{
+    public class Employee
+    {
+        [Required]
+        [Display(Name = "Employee Id")]
+        [DataType(DataType.Text)]
+
+        public int Id { get; set; }
+
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Employee Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(minimum:18, maximum:59)]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Email Adress")]
+        [MaxLength(40)]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Date of Joining")]
+        public DateTime Doj { get; set; }
+
+        [Required]
+        public int Salary { get; set; }
+    }
+}
+
+                    /*  Data Annotations
+
+                    In MVC when we want to apply validation to the view page, we will apply validations using model class , i.e. with help of Data Annotations.
+
+                    We can categories the validation in 3 ways
+                    Required → fields which are required , can not be blank or empty 
+		                    Example → name,email,contact, 
+                    Pattern → need to accept values based on predefined patterns or rules
+                                    Example – >email id format, pwd format, postal code
+                    Range → accept values within the range
+                                   Example age -> 18 - 60   min price =0  max price -2000
+
+                    How to apply data annotations in MVC
+                    I.e. with the help of attributes. Attributes will be applied using [ ]
+
+                     */
